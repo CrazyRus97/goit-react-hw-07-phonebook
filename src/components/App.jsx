d58e857a281d5b81f3a3b56296c881f79f8dfd9f
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-11-28 14:34:35 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
+import { useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { GlobalStyle } from 'styles/GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { Section } from './Section/Section';
+import { Title } from './Title/Title';
+import { Filter } from './Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+
+import { getContacts } from 'redux/contacts/contacts-selectors';
+
 export const App = () => {
+  const contacts = useSelector(getContacts);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Section title="PhoneBook">
+        <ContactForm />
+        {contacts.length > 0 && (
+          <>
+            <Title title="Contacts" />
+            <Filter />
+            <ContactList />
+          </>
+        )}
+      </Section>
+      <ToastContainer />
+      <GlobalStyle />
+    </Layout>
   );
 };
+
+// npm i prop-types
+// npm i react-toastify
+// npm i nanoid
+// npm i react
+// npm i react-icons
+// npm i yup
+// npm i formik
+// npm i styled-components
+// npm i modern-normalize
+// npm i react-redux
---
+++ src/styles/theme.jsx
@@ -0,0 +1,46 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#090430',
+    text: '#090430',
+    background: '#E0ABAD',
+    primary: '#0b139f',
+    secondary: '#a4d7fa',
+    accent: '#fcfdd7',
+    muted: '#cfc90b',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+  shadows: {
+    textShadow: '0 0 0 #090430',
+    boxShadow: 'rgb(0 0 0 / 24%) 3px 3px 2px 0px',
+  },
+};
