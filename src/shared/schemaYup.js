7fbc0792249db983d5c9fc6d9e46df5b0cd733cc
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-12-10 19:33:05 +0200

    yup

--- src/shared/schemaYup.js
+++ src/shared/schemaYup.js
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
 
+
 export const schema = yup.object().shape({
   avatar: yup.string(),
   name: yup
@@ -7,10 +8,10 @@ export const schema = yup.object().shape({
     .trim()
     .matches(
       /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-      'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d`Artagnan'
+      'Name may contain only letters, apostrophe, dash and spaces.'
     )
     .required(),
-  phone: yup
+  number: yup
     .string()
     .matches(
       /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
